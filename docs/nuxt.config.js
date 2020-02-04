74fd845eded37299ebedab65e58e299415acc147
BootstrapVue <vuebootstrap@gmail.com>
2020-02-04 12:09:10 +0530

    theme color changed

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -192,7 +192,7 @@ module.exports = {
       name: 'BootstrapVue',
       short_name: 'BootstrapVue',
       description: 'Quickly integrate Bootstrap v4 components with Vue.js',
-      theme_color: '#563d7c'
+      theme_color: '#322d59'
     },
     meta: {
       // `ogHost` is required for `og:image` to be populated
